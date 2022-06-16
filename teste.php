5103fec988761a34ff96de998eecc5c2f7cd63f0
Rebeca Bitner <rebecabitner@gmail.com>
2022-06-15 21:36:18 -0300

    Update teste.php

--- teste.php
+++ teste.php
@@ -1,13 +1,22 @@
 <?php
 
 class Pessoa{
-    public $nome
+    public $nome;
+    public $idade;
     public function falarNome(){
         echo $this->nome;
+    }
+    public function falarIdade(){
+        echo $this->idade;
     }
 }
 
 $uma_pessoa = new Pessoa;
-$uma_pessoa->nome = "Lucas";
+$uma_pessoa->nome = "Rebeca";
+$uma_pessoa->idade = "23";
+
+$uma_pessoa->falarNome();
+$uma_pessoa->falarIdade();
+
 
 ?>
